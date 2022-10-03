f597949cfb496b9af1f05a97c2b87a6da6c756a1
drewingtonC137 <amjj.edwards@gmail.com>
2022-10-03 15:32:10 +0200

    OnPost to Checkout Page

--- PhoneShop/Pages/Forms/CustomPhone.cshtml.cs
+++ PhoneShop/Pages/Forms/CustomPhone.cshtml.cs
@@ -8,8 +8,23 @@ namespace PhoneShop.Pages.Forms
     {
         [BindProperty]
         public PhonesModel Phone { get; set; }
+
+        public float PhonePrice { get; set; }
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            PhonePrice = Phone.BasePrice;
+            if (Phone.OledPanel) PhonePrice += 100;
+            if (Phone.HighRefresh) PhonePrice += 100;
+            if (Phone.BatteryUpgrade) PhonePrice += 50;
+            if (Phone.MemoryUpgrade) PhonePrice += 150;
+            if (Phone.ProModel) PhonePrice += 300;
+            Console.WriteLine(PhonePrice);
+            return RedirectToPage("/Checkout/Checkout", new {Phone.PhoneName, PhonePrice });
+            
         }
     }
 }
